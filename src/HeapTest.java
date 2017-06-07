83b1c937426db45e623a6d5e2feb834e9e8cc487
Thanh <thanhvv@gmail.com>
2017-06-07 16:09:27 -0700

    Sort k increasing decreasing array using heap.

--- src/HeapTest.java
+++ src/HeapTest.java
@@ -1,5 +1,5 @@
 import org.junit.*;
-import org.junit.Assert.*;
+import static org.junit.Assert.*;
 import java.util.*;
 
 public class HeapTest {
@@ -11,5 +11,35 @@ public class HeapTest {
 			System.out.print(val + " ");
 		}
 		System.out.println();
+	}
+	
+	@Test
+	public void test_sort_k_up_down_array() {
+		/* Test case 1 */
+		Integer[] a = {57, 131, 493, 294, 221, 339, 418, 452, 442, 190};
+		Integer[] b = HeapUtil.sort_k_up_down_array(a, 4);
+		assertEquals(b[0].intValue(), 57);
+		assertEquals(b[1].intValue(), 131);
+		assertEquals(b[2].intValue(), 190);
+		assertEquals(b[3].intValue(), 221);
+		assertEquals(b[4].intValue(), 294);
+		assertEquals(b[5].intValue(), 339);
+		assertEquals(b[6].intValue(), 418);
+		assertEquals(b[7].intValue(), 442);
+		assertEquals(b[8].intValue(), 452);
+		assertEquals(b[9].intValue(), 493);
+		
+		/*Test case 2 */
+		Integer[] c = {5, 6, 4, 3, 8, 9, 1, 2, 7};
+		Integer[] d = HeapUtil.sort_k_up_down_array(c, 5);
+		assertEquals(d[0].intValue(), 1);
+		assertEquals(d[1].intValue(), 2);
+		assertEquals(d[2].intValue(), 3);
+		assertEquals(d[3].intValue(), 4);
+		assertEquals(d[4].intValue(), 5);
+		assertEquals(d[5].intValue(), 6);
+		assertEquals(d[6].intValue(), 7);
+		assertEquals(d[7].intValue(), 8);
+		assertEquals(d[8].intValue(), 9);
 	}
 }
--- src/HeapUtil.java
+++ src/HeapUtil.java
@@ -1,14 +1,10 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class HeapUtil {	
+	
 	public static<T extends Comparable> List<T> merge_arrays(T[][] S) {
-		Comparator<Pair<T, Integer>> comparator = 
-				new Comparator<Pair<T, Integer>>() {
-			@Override
-			public int compare(Pair<T, Integer> p1, Pair<T, Integer> p2) {
-				return (p1.first.compareTo(p2.first));
-			}
-		};
+		Comparator<Pair<T, Integer>> comparator = (p1, p2) -> p1.first.compareTo(p2.first);
 		PriorityQueue<Pair<T, Integer>> min_heap = new PriorityQueue<Pair<T, Integer>>(10, comparator);
 		int[] S_idx = new int[S.length];
 		for (int i = 0; i < S.length; i++) {
@@ -27,5 +23,53 @@ public class HeapUtil {
 			}
 		}
 		return ret;		
+	}
+	
+	public static<T extends Comparable> T[] sort_k_up_down_array(T[] a, int k) {
+		int n = (k +1)/2;
+		int[] low = new int[n];
+		int[] high = new int[n];
+		int j = 0;
+		for (int i = 0; i < n; i++) {
+			low[i] = j;
+			while (j < (a.length -1) && a[j].compareTo(a[j+1]) <= 0) {
+				j++;
+			}
+			while (j < (a.length -1) && a[j].compareTo(a[j+1]) >= 0) {
+				j++;
+			}
+			high[i] = (i < n-1) ? j -1 : j;			
+		}
+		T[] b = (T[])Array.newInstance(a.getClass().getComponentType(), a.length);
+		Comparator<Pair<T, Integer>> comparator = (p1, p2) -> p1.first.compareTo(p2.first);
+		PriorityQueue<Pair<T, Integer>> min_heap = new PriorityQueue<Pair<T, Integer>>(n, comparator);
+		
+		/* Initialize the heap with the minimum values at both ends */
+		for (int i = 0; i < n; i++) {
+			if (a[low[i]].compareTo(a[high[i]]) <= 0) {
+				min_heap.add(new Pair(a[low[i]],i));
+				low[i]++;
+			} else {
+				min_heap.add(new Pair(a[high[i]], i));
+				high[i]--;
+			}
+		}
+		
+		int idx = 0;
+		while (idx < a.length && !min_heap.isEmpty()) {
+			Pair<T, Integer> p = min_heap.poll();
+			b[idx++] = p.first;
+			int pos = p.second.intValue();
+			if (low[pos] <= high[pos]) {
+				if (a[low[pos]].compareTo(a[high[pos]]) <= 0) {
+					min_heap.add(new Pair(a[low[pos]], pos));
+					low[pos]++;
+				} else {
+					min_heap.add(new Pair(a[high[pos]], pos));
+					high[pos]--;
+				}
+			}
+		}
+		return b;
 	}
 }
